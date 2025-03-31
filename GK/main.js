79e5e18eeaefc50c6ff7e264a252ed09781210f1
Tiencodedao <163629736+Tiencodedao@users.noreply.github.com>
2025-03-31 18:54:52 +0000

    Cap nhat

---
+++ GK/main.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu-button').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('hidden');
+  });
